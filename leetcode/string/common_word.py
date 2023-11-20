4e25150b6ce0176d1dd9a68a5643bb97abaa8436
ByeongJun Kim <chchdelm3@icloud.com>
2023-11-20 17:26:18 +0900

    Counter 객체 사용 방법 추가

--- leetcode/string/common_word.py
+++ leetcode/string/common_word.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/most-common-word/
+import re
+from collections import Counter, defaultdict
 from typing import List
 
 
@@ -21,9 +23,19 @@ class Solution:
 
         return max(count_word.items(), key=lambda x: x[1])[0]
 
+    # Counter 사용
+    def mostCommonWord_simple(self, paragraph: str, banned: List[str]) -> str:
+        words = [
+            word
+            for word in re.sub(r"[^\w]", " ", paragraph).lower().split()
+            if word not in banned
+        ]
+        counts = Counter(words)
+        return counts.most_common(1)[0][0]
+
 
 # paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 paragraph = "a, a, a, a, b,b,b,c, c"
 banned = ["a"]
 
-print(Solution().mostCommonWord(paragraph, banned))
+print(Solution().mostCommonWord_simple(paragraph, banned))
